5ee575f1d87ffbd1bb1fd57ce7ddca740f6a9edc
Paper99 <1398872296@qq.com>
2024-01-22 14:01:28 +0800

    1.1 onMainGroupMessage

--- src/main/java/cn/paper_card/qq_group_member_info/api/QqGroupMemberInfoApi.java
+++ src/main/java/cn/paper_card/qq_group_member_info/api/QqGroupMemberInfoApi.java
@@ -1,8 +1,11 @@
 package cn.paper_card.qq_group_member_info.api;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("unused")
 public interface QqGroupMemberInfoApi {
     @NotNull QqGroupMemberInfoService getQqGroupMemberInfoService();
+
+    @Nullable String onMainGroupMessage(long senderQq, @NotNull String senderName, @NotNull String message) throws Exception;
 }
